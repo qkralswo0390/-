d971fcb3deb4a285e6b32aa9d073a12248bcce88
harim <p66389416@gmail.com>
2025-06-05 16:20:02 +0900

    akzj

--- app/src/main/java/com/example/project/jeongwang.kt
+++ app/src/main/java/com/example/project/jeongwang.kt
@@ -32,7 +32,6 @@ class jeongwang : AppCompatActivity(), OnMapReadyCallback {
         val customView = layoutInflater.inflate(R.layout.jeongwang1_toolbar_layout, null)
         binding.toolbar.addView(customView)
 
-// 뒤로가기 버튼
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         binding.toolbar.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
@@ -43,10 +42,9 @@ class jeongwang : AppCompatActivity(), OnMapReadyCallback {
 
         // 1) 새로운 리스트 생성
         val newList = listOf(
-            ListItem("토비공원", R.drawable.intersect),
-            ListItem("정왕역", R.drawable.intersect),
-            ListItem("아파트 단지", R.drawable.intersect),
-            ListItem("공원 산책로", R.drawable.intersect),
+            ListItem("젊음과 패기공원", R.drawable.intersect),
+            ListItem("정왕1공원", R.drawable.intersect),
+            ListItem("정왕2공원", R.drawable.intersect),
         )
 
         // 2) 리사이클러뷰 레이아웃 매니저 설정
@@ -60,12 +58,9 @@ class jeongwang : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        mMap.setMapStyle(
-            MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style)
-        )
+        mMap.setMapStyle( MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
 
         val layer = GeoJsonLayer(mMap, R.raw.jeongwang1dong_only, this)
-
         for (feature in layer.features) {
             val polygonStyle = GeoJsonPolygonStyle().apply {
                 strokeColor = Color.RED        // 경계선 색
@@ -73,14 +68,12 @@ class jeongwang : AppCompatActivity(), OnMapReadyCallback {
             }
             feature.polygonStyle = polygonStyle
         }
-
         layer.addLayerToMap()
 
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.setMinZoomPreference(14f)
 
         val jeongwang1dong = LatLng(37.3365, 126.7335)
-
         val bounds = LatLngBounds(
             LatLng(37.32, 126.71),  // 남서
             LatLng(37.35, 126.75)   // 북동
@@ -88,6 +81,26 @@ class jeongwang : AppCompatActivity(), OnMapReadyCallback {
 
         mMap.setLatLngBoundsForCameraTarget(bounds)
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(jeongwang1dong, 14f))
+
+        val parkMarkers = listOf(
+            Pair("젊음과 패기공원", LatLng(37.348258, 126.740075)),
+        )
+
+        for ((title, latLng) in parkMarkers) {
+            val marker = mMap.addMarker(
+                com.google.android.gms.maps.model.MarkerOptions()
+                    .position(latLng)
+            )
+            marker?.tag = title
+        }
+
+        mMap.setOnMarkerClickListener { marker ->
+            val parkName = marker.tag as? String
+            if (parkName != null) {
+                Toast.makeText(this, "$parkName 클릭됨", Toast.LENGTH_SHORT).show()
+            }
+            true // true로 반환해서 기본 동작 차단
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
